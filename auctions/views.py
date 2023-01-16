c3b4d0888f76a3efafe3e0c46da1559be5d5907b
GaganGanapathy <109824692+GaganGanapathy@users.noreply.github.com>
2023-01-16 13:54:55 +0530

    added listing

--- auctions/views.py
+++ auctions/views.py
@@ -4,15 +4,46 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import User, Category, Listing
 
 
 def index(request):
-    return render(request, "auctions/index.html")
+    activeListings = Listing.objects.filter(isActive=True)
+    allCategories = Category.objects.all()
+    return render(request, "auctions/index.html",{
+        "listings": activeListings,
+        "categories": allCategories,
+    })
 
 def createListing(request):
     if request.method == "GET":
-        return render(request, "auctions/create.html")
+        allCategories = Category.objects.all()
+        return render(request, "auctions/create.html",{
+            "categories": allCategories
+        })
+    else:
+        title = request.POST["title"]
+        description = request.POST["description"]
+        imageurl = request.POST["imageurl"]
+        price = request.POST["price"]
+        category = request.POST["category"]
+
+        currentUser = request.user
+
+        categoryData = Category.objects.get(categoryName=category)
+
+        newListing = Listing(
+            title=title,
+            description=description,
+            imageUrl=imageurl,
+            price=float(price),
+            category=categoryData,
+            owner=currentUser
+        )
+
+        newListing.save()
+
+        return HttpResponseRedirect(reverse(index))
 
 def login_view(request):
     if request.method == "POST":
